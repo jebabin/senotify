22d52c9f0a7cc44707921ca4ae0ecc0ad8bc733d
radius <radius@caf82c9d-cf78-e6d7-1708-7086859a1f78>
2010-06-21 20:47:36 +0000

    - Add proxy support - Add Windows growl support - Use API Key - Provide logo and suppress --image option - Add question URL to terminal output - Better query to API - Show number of new question in terminal output - Add view count to output - Support multiple --site argument to monitor many site at once - Support tag exclusion with --excludetag

--- SENotify.pl
+++ SENotify.pl
@@ -16,9 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Todo:
-#	- Filters
-#	- Use Mac::Growl for Growl
-#	- Support other Notificaton system
+#	- Improve tag filter
+#	- Support other notificaton system
 
 # For history see the end of the script
 
@@ -30,83 +29,112 @@ use Data::Dumper;
 use Getopt::Long;
 use Compress::Zlib;
 use LWP::UserAgent;
+use File::Basename;
+use File::Spec::Functions qw(rel2abs);
 
 my %default_config;
 my %opts;
 
-GetOptions(\%opts, 'image|i=s', 'site|s=s', 'refresh|r=i', 'growl|g', 'help|h');
+###### Configuration
 
-$default_config{'site'} = 'serverfault'; # serverfault | stackoverflow | superuser | meta.stackoverflow | stackapps
+my $key = 'W2cJqWcKa0OFBM6OCsS2JA';
+my $growlnotifypath = '"C:\Program Files\Growl for Windows\growlnotify"'; # For windows only
+
+
+$default_config{'site'} = ['serverfault', 'superuser']; # serverfault | stackoverflow | superuser | meta.stackoverflow | stackapps
 $default_config{'refresh'} = '300';
 $default_config{'growl'} = '0';
-$default_config{'image'} = '';
+$default_config{'excludetag'} = [];
 
-$opts{'site'} = $opts{'site'}       || $default_config{'site'};
-$opts{'refresh'} = $opts{'refresh'} || $default_config{'refresh'};
-$opts{'growl'} = $opts{'growl'}     || $default_config{'growl'};
-$opts{'image'} = $opts{'image'}     || $default_config{'image'};
+###### Do not edit bellow
+
+GetOptions(\%opts, 'site|s=s@', 'excludetag|e=s@', 'refresh|r=i', 'growl|g', 'help|h');
+
+$opts{'site'} = $opts{'site'}               || $default_config{'site'};
+$opts{'refresh'} = $opts{'refresh'}         || $default_config{'refresh'};
+$opts{'growl'} = $opts{'growl'}             || $default_config{'growl'};
+$opts{'excludetag'} = $opts{'excludetag'}   || $default_config{'excludetag'};
 
 if (defined($opts{'help'})) {
-	print STDERR "Usage: $0 [-s site] [-r refresh] [-g] [-i image]\n";
-	print STDERR " -s|--site : Site to monitor, one of serverfault | stackoverflow | superuser | meta.stackoverflow | stackapps, default to ".$default_config{'site'}."\n";
-	print STDERR " -r|--refresh : Refresh rate in seconds, default to 300 seconds\n";
-	print STDERR " -g|--growl : Enable growl notification (need growlnotify)\n";
-	print STDERR " -i|--image : Image to use for growl notification\n";
+	print STDERR "Usage: $0 [-r refresh] [-g] [[-e tag] [-e tag] ... ] [[-s site] [-s site] ... ]\n";
+	print STDERR "    -s,--site        Site to monitor, one of serverfault | stackoverflow | superuser | meta.stackoverflow | stackapps\n";
+	print STDERR "                     default to ".join(',',@{$default_config{'site'}})."\n";
+	print STDERR "                     can be repeated many time\n";
+	print STDERR "    -g,--growl       Enable growl notification (need growlnotify)\n";
+	print STDERR "                     Growl for mac : http://growl.info/\n";
+	print STDERR "                     Growl for windows : http://www.growlforwindows.com/\n";
+	print STDERR "    -e,--excludetag  Exclude question contains these tags\n";
+	print STDERR "                     can be repeated many time\n";
+	print STDERR "    -r,--refresh     Refresh rate in seconds, default to 300 seconds\n";
 	exit;
 }
 
+if ($opts{'growl'} && ($^O ne 'darwin') && ($^O ne 'MSWin32')) {
+	warn "growl can only be used on Mac OS X and Windows";
+}
+
 my $ua = LWP::UserAgent->new;
-$ua->agent("SEPNotification/0.1");
+$ua->agent("SENotify/0.2");
+$ua->env_proxy();
 
-my $req = HTTP::Request->new(GET => 'http://api.'.$opts{'site'}.'.com/0.8/questions?sort=creation&pagesize=1');
-my $res = $ua->request($req);
-
-my $lastquestion = 0;
-if ($res->is_success) {
-		my $unzipped = Compress::Zlib::memGunzip($res->content); 
-		my $result = decode_json($unzipped);
-		$lastquestion = $result->{questions}[0]{question_id};;
-} else {
-	die "Unable to retrieve data (".$res->status_line.")";
+my %lastquestionts;
+foreach my $site (@{$opts{'site'}}) {
+	$lastquestionts{$site} = time;
 }
 
 while (sleep $opts{'refresh'}) {
-	my $req = HTTP::Request->new(GET => 'http://api.'.$opts{'site'}.'.com/0.8/questions?sort=creation&pagesize=10');
-	my $res = $ua->request($req);
+	foreach my $site (@{$opts{'site'}}) {
+		my $moredata = 1;
+		my $i = 0;
+		while($moredata) {
+			$i++;
+			print 'http://api.'.$site.'.com/0.8/questions?sort=creation&order=asc&fromdate='.$lastquestionts{$site}.'&pagesize=10&page=1&key='.$key . "\n";
+			my $req = HTTP::Request->new(GET => 'http://api.'.$site.'.com/0.8/questions?sort=creation&order=asc&fromdate='.$lastquestionts{$site}.'&pagesize=10&page=1&key='.$key);
+			my $res = $ua->request($req);
+		
+			if ($res->is_success) {
+				my $unzipped = Compress::Zlib::memGunzip($res->content); 
+				my $result = decode_json($unzipped);
+	
+				$moredata = 0 if ($result->{'total'} <= $result->{'pagesize'});
+				print $result->{'total'} . " question found on $site at : " . localtime() . "\n\n" if ($i == 1);
+		
+				foreach my $i (@{$result->{questions}}) {
+					my $skip = 0;
 
-	if ($res->is_success) {
-		my $unzipped = Compress::Zlib::memGunzip($res->content); 
-		my $result = decode_json($unzipped);
-
-		my $current_lastquestion = $result->{questions}[0]{question_id};
-
-		print "Question found at : " . localtime() . "\n\n";
-
-		foreach my $i (@{$result->{questions}}) {
-			if ($i->{question_id} == $lastquestion) {
-				last;
-			} else {
-				if ($opts{'growl'}) {
-					if ($opts{'image'} ne '') {
-						open(OUT, '| growlnotify --image '.$opts{'image'}) or warn "Error: Couldn't open the pipe to growlnotify $!";
-					} else {
-						open(OUT, '| growlnotify') or warn "Error: Couldn't open the pipe to growlnotify $!";							
+					foreach my $qtag (@{$i->{'tags'}}) {
+						foreach my $excludedtag (@{$opts{'excludetag'}}) {
+							$skip = 1 if ($excludedtag eq $qtag);
+						}
 					}
-					print OUT '('. $i->{answer_count} . ') ' .$i->{title} . "\n";
-					print OUT '['.join('] [',@{$i->{tags}}).']' . "\n";
-					close(OUT);
-				}
-				print '('. $i->{answer_count} . ') ' .$i->{title} . "\n";
-				print '['.join('] [',@{$i->{tags}}).']' . "\n";
-				print "\n";
 
+					$lastquestionts{$site} = $i->{creation_date} + 1; # Bad Hack - Could make question to be lost if two question has same creation_id			
+
+					next if $skip;
+					
+					if ($opts{'growl'}) {
+						if ($^O eq 'darwin') {
+							open(OUT, '| growlnotify -n SENotify --image '.dirname(rel2abs($0)).'/logo/'.$site.'.png') or warn "Error: Couldn't open the pipe to growlnotify $!";
+							print OUT '(v:'. $i->{view_count} .'|a:'. $i->{answer_count} . ') ' .$i->{title} . "\n";
+							print OUT '['.join('] [',@{$i->{tags}}).']' . "\n";
+							close(OUT);
+						} elsif ($^O eq 'MSWin32')  {
+							my $message = '"(v:'. $i->{view_count} .'|a:'. $i->{answer_count} . ') ' .$i->{title} . '\n['.join('] [',@{$i->{tags}}).']"';
+							system($growlnotifypath, '/i:'.dirname(rel2abs($0)).'/logo/'.$site.'.png', $message);	
+						}
+					}
+										
+					print '(v:'. $i->{view_count} .'|a:'. $i->{answer_count} . ') ' .$i->{title} . "\n";
+					print '['.join('] [',@{$i->{tags}}).']' . "\n";
+					print 'http://www.'.$site.'.com/questions/'.$i->{question_id}."\n";
+					print "\n";
+		
+				}
+			} else {
+				warn "Unable to retrieve data (".$res->status_line.")";
+				$moredata = 0;
 			}
 		}
-
-		$lastquestion = $current_lastquestion;
-
-	} else {
-		warn "Unable to retrieve data (".$res->status_line.")";
 	}
 }
 
@@ -114,5 +142,17 @@ __END__
 
 History (not following commit revision)
 
-Revision 0.1 2010/06/21
+Revision 0.2 - 2010/06/22
+- Add proxy support
+- Add Windows growl support
+- Use API Key
+- Provide logo and suppress --image option
+- Add question URL to terminal output
+- Better query to API
+- Show number of new question in terminal output
+- Add view count to output
+- Support multiple --site argument to monitor many site at once
+- Support tag exclusion with --excludetag
+
+Revision 0.1 - 2010/06/21
 - Initial commit
